3329a5d0c936f93643704701652a6d7bb92f80c7
fournierclement <fournier.clt@gmail.com>
2016-04-20 13:25:44 +0200

    step 1.2

--- scr/scores/TestHighScore1.java
+++ scr/scores/TestHighScore1.java
@@ -10,9 +10,44 @@ import java.util.*;
 public class TestHighScore1 {
 
     public static void main(String [] args) {
+        askName();
+        readScores();
+    }
+
+    public static void readScores() {
+        Scanner keyboard = new Scanner(System.in);
+        //While the File isn't open, try again.
+        boolean done = false;
+        FileInputStream scoreFile = null;
+        while (! done) {
+            done = true;
+            //Asking for the filepath.
+            System.out.println("Emplacement du fichier :\n");
+            String filePath ="";
+            //while (keyboard.hasNextLine()){
+                filePath = keyboard.nextLine();
+            //}
+            //Open the file.
+            try{
+                scoreFile = new FileInputStream(new File(filePath));
+            }
+            catch (FileNotFoundException e) {
+                done = false;
+                System.out.println(e);
+            }
+        }
+        try {scoreFile.close();} catch (IOException e) {}
+        
+        //keyboard.close();
+    }
+    
+    public static void askName() {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("What's ur name?");
-        String name = keyboard.nextLine();
+        String name ="";
+        //while (!keyboard.hasNextLine()){
+            name = keyboard.nextLine();
+        //}
         System.out.println(name);
     }
 }
